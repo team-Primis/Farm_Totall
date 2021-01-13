ee8b146346c634a8b1384c3782630a53c2b66a56
7897-ee <7897syle@gmail.com>
2021-01-13 14:24:06 +0900

    아 레이캐스트

--- Scripts/SpawningDirt.cs
+++ Scripts/SpawningDirt.cs
@@ -5,25 +5,48 @@ using UnityEngine;
 public class SpawningDirt : MonoBehaviour
 {
     public GameObject DirtPrefab;//파낸 검은 흙 오브젝트 필드 설정.
-    public Transform target;//플레이어의 현재 위치를 가져오기 위한 필드.
+    public GameObject thePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//게임플레이화면에서의 마우스 위치를 VEctor2 타입의 마우스 위치에 배정.
-        mousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기만큼 움직이는 것처럼 보이기 위해 반올림하여 마우스 위치 재배정.
-        
-        if (Mathf.Abs(target.position.x) <= 1.5f || Mathf.Abs(target.position.y) <= 2f)//현재 플레이어 위치를 기준으로 주변 x로는 타일 1.5칸, y로는 2칸 이하에서
-        { if (Input.GetMouseButton(0))//마우스 왼클릭을 하면
-            {
-                GameObject DarkDirt = Instantiate(DirtPrefab);//땅이 파짐
-                DarkDirt.transform.position = mousePosition;//파진 땅 위치는 클릭한 마우스 위치.
-            }
-            }
+
+        SpawnDirt();
+
+    }
+    private void SpawnDirt()
+    {
+        Vector2 theplayerPosition = thePlayer.transform.position;//게임플레이화면에서의 마우스 위치를 Vector2 타입의 마우스 위치에 배정.
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 themousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기마다 이동하는 것처럼 보이기 위해 올림하여 마우스 위치 재설정.
+        Vector2 distance = theplayerPosition - mousePosition;
+
+        if (Input.GetMouseButton(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//카메라에서 레이저를 스크린상에서의 마우스 위치에서 발사함.
+            RaycastHit2D hit=Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            
+                if (hit.transform == null || hit.transform.CompareTag("Player"))
+                {
+                    
+                    if (Mathf.Abs(distance.x) <= 1.5f && Mathf.Abs(distance.y) <= 2f)//마우스 왼클릭을 하는 중에는
+                    {
+
+                        GameObject DarkDirt = Instantiate(DirtPrefab);//식물 생성
+                        DarkDirt.transform.position = themousePosition;//생성한 식물을 마우스 위치와 같은 곳에 배치함.
+
+                    }
+                }
+
+
+               
+        }
     }
 }
--- Scripts/SpawningPlant.cs
+++ Scripts/SpawningPlant.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SpawningPlant : MonoBehaviour//식물 spawning하는 클래스
 {
     public GameObject[] PlantPrefabs;//배열로 구현하여 인덱스가 0일 때 꽃, 1일 때 호박을 심도록 구현.
-    public Transform target;//플레이어의 현재 위치 추적하기 위한 필드.
-  
+    public GameObject thePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,25 +16,48 @@ public class SpawningPlant : MonoBehaviour//식물 spawning하는 클래스
     // Update is called once per frame
     void Update()
     {
-        
+
         SpawnPlant(PlantPrefabs[0]);//심을 식물 종류를 어떻게 결정할 수 있을지 몰라서 우선 꽃만 키울 수 있게 해둠. 
     }
 
     void SpawnPlant(GameObject PlantPrefabs)//식물 심는 함수.
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//게임플레이화면에서의 마우스 위치를 Vector2 타입의 마우스 위치에 배정.
-        mousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기마다 이동하는 것처럼 보이기 위해 올림하여 마우스 위치 재설정.
-        if (Mathf.Abs(target.position.x) <= 1.5f || Mathf.Abs(target.position.y) <= 2f)//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
-        {
-            if (Input.GetMouseButton(0))//마우스 왼클릭을 하는 중에는
-            {
-                
-                GameObject PlantedPlant = Instantiate(PlantPrefabs);//식물 생성
-                PlantedPlant.transform.position = mousePosition;//생성한 식물을 마우스 위치와 같은 곳에 배치함.
-               
-            }
-            }
-        }
-    }
 
+        Vector2 theplayerPosition = thePlayer.transform.position;//게임플레이화면에서의 마우스 위치를 Vector2 타입의 마우스 위치에 배정.
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 themousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기마다 이동하는 것처럼 보이기 위해 올림하여 마우스 위치 재설정.
+        Vector2 distance = theplayerPosition - mousePosition;
+         
+        if (Input.GetMouseButton(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//카메라에서 레이저를 스크린상에서의 마우스 위치에서 발사함.
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+    
+                if (hit)
+                {
+                
+                    if(hit.transform.tag == "DarkDirt")
+                    {
+                        if (Mathf.Abs(distance.x) <= 1.5f && Mathf.Abs(distance.y) <= 2f)//마우스 왼클릭을 하는 중에는
+                        {
+
+                            GameObject PlantedPlant = Instantiate(PlantPrefabs);//식물 생성
+                            PlantedPlant.transform.position = themousePosition;//생성한 식물을 마우스 위치와 같은 곳에 배치함.
+
+                        }
+                    }
+
+
+                    
+                }
+
+            
+            
+            }
+            
+        }
+   
 }
+
+
