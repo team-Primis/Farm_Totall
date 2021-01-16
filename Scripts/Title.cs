f35316c93e3402f9695313d43eb8ff8a855f01de
kate1301@naver.com <kate1301@naver.com>
2021-01-16 23:33:25 +0900

    타이틀 UI 추가

---
+++ Scripts/Title.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    public Image NewBefore; // 기존에 존제하는 이미지
+    public Sprite NewAfter; // 바뀌어질 이미지
+    public Image ContiBefore;
+    public Sprite ContiAfter;
+
+    // Start is called before the first frame update
+    void Start()
+    {   }
+
+    // Update is called once per frame
+    void Update()
+    {   }
+
+    public void ChangeNewImage()
+    {
+        NewBefore.sprite = NewAfter;
+    }
+
+    public void ChangeContiImage()
+    {
+        ContiBefore.sprite = ContiAfter;
+    }
+}
