57a77b988adb8ec55a5f3b2b40c205835ce85452
7897-ee <7897syle@gmail.com>
2021-01-19 21:52:41 +0900

    왜 남았지

--- Scripts/SpawningDirt.cs
+++ Scripts/SpawningDirt.cs
@@ -27,15 +27,15 @@ public class SpawningDirt : MonoBehaviour
         Vector2 themousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기마다 이동하는 것처럼 보이기 위해 올림하여 마우스 위치 재설정.
         Vector2 distance = theplayerPosition - mousePosition;
 
-        if (Input.GetMouseButton(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
+        if (Input.GetMouseButtonDown(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
         {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//카메라에서 레이저를 스크린상에서의 마우스 위치에서 발사함.
             RaycastHit2D hit=Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
             
-                if (hit.transform == null || hit.transform.CompareTag("Player"))
+                if (hit.transform == null || hit.collider.CompareTag("Player"))
                 {
-                    
+                Debug.Log("되네");
                     if (Mathf.Abs(distance.x) <= 1.5f && Mathf.Abs(distance.y) <= 2f)//마우스 왼클릭을 하는 중에는
                     {
 
--- Scripts/SpawningPlant.cs
+++ Scripts/SpawningPlant.cs
@@ -29,7 +29,7 @@ public class SpawningPlant : MonoBehaviour//식물 spawning하는 클래스
         Vector2 themousePosition = new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y));//타일 크기마다 이동하는 것처럼 보이기 위해 올림하여 마우스 위치 재설정.
         Vector2 distance = theplayerPosition - mousePosition;
         
-        if (Input.GetMouseButton(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
+        if (Input.GetMouseButtonDown(0))//플레이어의 위치를 기준으로 x는 타일 1.5칸, y는 타일 2칸 이하에서
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//카메라에서 레이저를 스크린상에서의 마우스 위치에서 발사함.
             RaycastHit2D[] hit = Physics2D.RaycastAll(ray.origin, ray.direction, Mathf.Infinity);
@@ -38,12 +38,12 @@ public class SpawningPlant : MonoBehaviour//식물 spawning하는 클래스
             {
                 for (int j = 0; j < hit.Length; j++)
                 {
-                    if (hit[i].transform.tag == "DarkDirt")
+                    if (hit[i].transform.tag == "DarkDirt" && hit[j].transform.tag!="Plant")
                     {
-                        
 
-                        if (hit[j].transform.tag != "Plant")
-                        {
+
+                        Debug.Log(hit[j].transform.tag);
+                        
 
                             if (Mathf.Abs(distance.x) <= 1.5f && Mathf.Abs(distance.y) <= 2f)//마우스 왼클릭을 하는 중에는
                             {
@@ -52,7 +52,7 @@ public class SpawningPlant : MonoBehaviour//식물 spawning하는 클래스
                                 PlantedPlant.transform.position = themousePosition;//생성한 식물을 마우스 위치와 같은 곳에 배치함.
 
                             }
-                        }
+                        
 
                     }
                 }
