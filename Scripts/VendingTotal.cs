678b8f05391d5ca22fb28e3c0c4914633c4bbb07
smilehae <mihaepyun@gmail.com>
2021-01-19 13:50:15 +0900

    자판기와 숫자 연동 완료

---
+++ Scripts/VendingTotal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VendingTotal : MonoBehaviour
+{
+    private PlayerControll playerScript;
+    private inventory inven;
+    public Text seedNameText;
+    public Text moneyText;
+    public Text buyNumText;
+
+    public int seedMoney; //씨앗 구매시 필요한 돈 (구매창에 뜨는 용)
+    public int buyNum; // 현재 구매할 씨앗 창 (구매창 표시)
+    public string seedName; //현재 구매할 씨앗 이름
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").GetComponent<PlayerControll>();
+        inven = GameObject.Find("Inventory").GetComponent<inventory>();
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
---
+++ Scripts/VendorItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VendorItem : MonoBehaviour, IPointerClickHandler
+{
+    private VendingTotal vendingScript;
+    private itemDatabase db;
+
+    private Image buyImg;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vendingScript = GameObject.Find("vendingPanel").GetComponent<VendingTotal>();
+        db = GameObject.Find("Database").GetComponent<itemDatabase>();
+        buyImg = GameObject.Find("seedImage").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //각 아이템이 클릭되면, 태그에 맞는 db에 저장된 객체 가져옴!
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        string itemTag = gameObject.tag;
+        Item ClickedItem = db.GetItem(itemTag);
+        if (ClickedItem == null)
+        {
+            Debug.Log("해당 이름의 아이템은 db에 존재하지 않습니다.");
+        }
+        else {
+            Debug.Log("item의 태그는 " + itemTag + "입니다.");
+            vendingScript.seedName = itemTag;
+            vendingScript.seedNameText.text = ClickedItem.Kname;
+
+            vendingScript.buyNum = 1;
+            vendingScript.buyNumText.text = 1.ToString();
+            // 구매창에 뜬 이미지를 클릭한 이미지의 아이콘으로 바꿔주기
+            buyImg.sprite = ClickedItem.icon; 
+            
+
+            foreach(KeyValuePair<string,int> keyValue in ClickedItem.stats)
+            {
+                if(keyValue.Key == "cost")
+                {
+                    Debug.Log(itemTag + "의 가격은 " + keyValue.Value + "입니다.");
+                    vendingScript.seedMoney = keyValue.Value;
+                    vendingScript.moneyText.text = keyValue.Value.ToString() + "원";
+                }
+            }
+        }
+    }
+}
