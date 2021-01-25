4b6c19d0d7e304e1c5610e8b93d52afcab953ff9
smilehae <mihaepyun@gmail.com>
2021-01-25 15:35:52 +0900

    아이템과 UI 시스템 연결 코드 획득 : FindGameObjectsWithTag 사용

--- Scripts/ContainerUI.cs
+++ Scripts/ContainerUI.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ContainerUI : MonoBehaviour
 {
-    public List<UIItem> container = new List<UIItem>();
+    public bool isItemEnrolled = false; //한번만 실행되게 하는 용 (containerUI랑 CUItem연결용) : 맨 처음에 시도할경우 CUItem이 setActive(false)라 반영이 안됨
+    public GameObject containerObj;
+    public List<CUIItem> container = new List<CUIItem>();
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (GameObject itemslots in GameObject.FindGameObjectsWithTag("containerSlot"))
+        
+    }
+
+    
+   void Update()
+    {
+        //한번도 실행된 적 없고, 현재 컨테이너가 멀쩡히 켜져있으면 한번 실행
+        
+        if (!isItemEnrolled && gameObject.activeSelf == true)
         {
-            //container.Add(itemslots.GetComponent<UIItem>);
+            foreach (GameObject itemslots in GameObject.FindGameObjectsWithTag("containerSlot"))
+            {
+                //GetComponent<>로 하니까 메소드를 바꿀 수 없습니다 뜸!
+                container.Add(itemslots.GetComponentInChildren<CUIItem>());
+            }
+
+            isItemEnrolled = true;
         }
+        
     }
 
     public void UpdateSlot(int slot, Item item)
