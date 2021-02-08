1c13496e5e7d612894763a302b938c5bb7576698
kate1301@naver.com <kate1301@naver.com>
2021-02-08 13:55:13 +0900

    타이틀 불러오기화면

--- Scripts/TitleManager.cs
+++ Scripts/TitleManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // 버튼
+using UnityEngine.UI; // 버튼, text 편집
 using UnityEngine.SceneManagement; // 씬 이동
 
 public class TitleManager : MonoBehaviour
@@ -10,6 +10,9 @@ public class TitleManager : MonoBehaviour
     public Sprite NewAfter; // 바뀌어질 이미지
     public Image ContiBefore;
     public Sprite ContiAfter;
+
+    public GameObject Choice;
+    public GameObject WarningTxt;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +25,15 @@ public class TitleManager : MonoBehaviour
     public void OnClickNewGame()
     {
         Debug.Log("새 게임");
-        NewBefore.sprite = NewAfter; // change button image
+        NewBefore.sprite = NewAfter; // change button image - 마우스 닿는 동안만으로 수정할 것
         SceneManager.LoadScene("OutSide");
     }
 
     public void OnClickContinue()
     {
         Debug.Log("이어하기");
-        ContiBefore.sprite = ContiAfter; // change button image
+        ContiBefore.sprite = ContiAfter; // change button image - 마찬가지
+        Choice.SetActive(true); // 파일 선택 창 띄우기
     }
 
     public void OnClickQuit()
@@ -40,5 +44,31 @@ public class TitleManager : MonoBehaviour
 #else        
         Application.Quit(); // 실행된 게임 프로그램 종료 - 에디터에선 동작 X
 #endif
+    }
+
+    public void OnClickX()
+    {
+        Debug.Log("이어하기 취소");
+        Choice.SetActive(false); // 파일 선택 창 끄기
+    }
+
+    public void OnClickC1()
+    {
+        //저장한 파일이 존재한다면
+        Debug.Log("File 1 이어하기");
+        //SceneManager.LoadScene("OutSide");
+        
+        //저장한 파일이 존재하지 않는다면
+        //WarningTxt.SetActive(true); // 몇 초 동안만
+    }
+
+    public void OnClickC2()
+    {
+        //저장한 파일이 존재한다면
+        Debug.Log("File 2 이어하기");
+        //SceneManager.LoadScene("OutSide");
+
+        //저장한 파일이 존재하지 않는다면
+        //WarningTxt.SetActive(true); // 몇 초 동안만
     }
 }
